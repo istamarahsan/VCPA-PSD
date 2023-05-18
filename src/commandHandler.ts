b6b043c728cb9b6bccea83ae186ab91bb2964c05
San <istamarahsan@gmail.com>
2023-05-18 11:29:08 +0700

    replace pushlog

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -6,7 +6,6 @@ import * as command_status from "./commands/status";
 import * as command_stop from "./commands/stop";
 import * as command_raisehand from "./commands/raisehand";
 import * as command_lowerhand from "./commands/lowerhand";
-import * as command_pushlogv2 from "./commands/pushlogv2";
 
 const commands = [
 	command_start,
@@ -14,8 +13,7 @@ const commands = [
 	command_stop,
 	command_pushlog,
 	command_raisehand,
-	command_lowerhand,
-	command_pushlogv2
+	command_lowerhand
 ];
 
 export async function register(client : Client) {
